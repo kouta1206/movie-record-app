e0ee4cd3a388732db40a54c61b28baa4f54b22e5
kouta1206 <ko.bump1206@icloud.com>
2022-10-15 22:50:42 +0900

    cssがコンパイルされない不具合修正

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,14 +1,11 @@
-import 'bootstrap';
-import '../stylesheets/application';
+/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within app/packs and only use these pack files to reference
 // that code so it'll be compiled.
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,3 +13,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Activate Active Storage
+// import * as ActiveStorage from "@rails/activestorage"
+// ActiveStorage.start()
